da86e65449cdb3cf53d7ae8d9c08549e87e9868b
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2022-11-30 09:31:50 +0200

    first commit with simple react and bootstrap design

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+  return (
+    <div>
+
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded" to={'/home'}>home</Link></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+    </div>
+  )
+}
+
+export default Navbar
